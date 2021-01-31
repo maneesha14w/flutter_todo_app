d5542a51e137120cf7f3ed032f369bf230ea3e95
maneesha14w <maneesha14w@gmail.com>
2021-01-31 22:56:22 +0530

    finished home page and added new screen

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -14,7 +14,7 @@ class _HomepageState extends State<Homepage> {
         child: Container(
           color: Color(0xFFF6F6F6),
           width: double.infinity,
-          padding: EdgeInsets.symmetric(horizontal: 24.0, vertical: 32.0),
+          padding: EdgeInsets.symmetric(horizontal: 24.0),
           child: Stack(
             children: [
               Column(
@@ -22,22 +22,33 @@ class _HomepageState extends State<Homepage> {
                 children: [
                   Container(
                     margin: EdgeInsets.only(
+                      top: 32.0,
                       bottom: 32.0,
                     ),
                     child: Image(
                       image: AssetImage('assets/images/logo.png'),
                     ),
                   ),
-                  TaskCardWidget(
-                    title: "Get Started",
-                    description:
-                        'Hello user! Welcome to my Todo app, this is a default todo task that you can edit, save or delete',
+                  Expanded(
+                    child: ListView(
+                      children: [
+                        TaskCardWidget(
+                          title: "Get Started",
+                          description:
+                          'Hello user! Welcome to my Todo app, this is a default todo task that you can edit, save or delete. Feel free to mess around and experiment with this todo to figure out how todos works.',
+                        ),
+                        TaskCardWidget(),
+                        TaskCardWidget(),
+                        TaskCardWidget(),
+                        TaskCardWidget(),
+                      ],
+                    ),
                   ),
-                  TaskCardWidget(),
+
                 ],
               ),
               Positioned(
-                bottom: 0.0,
+                bottom: 24.0,
                 right:0.0,
                 child: Container(
                   width: 60.0,
