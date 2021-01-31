ec6dac56454e8e862b764762eb8b66c91ab115a8
maneesha14w <maneesha14w@gmail.com>
2021-01-31 15:31:31 +0530

    changed colors added padding

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -12,9 +12,11 @@ class _HomepageState extends State<Homepage> {
     return Scaffold(
       body: SafeArea(
         child: Container(
+          color: Color(0xFFF6F6F6),
           width: double.infinity,
           padding: EdgeInsets.symmetric(horizontal: 24.0, vertical: 32.0),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Image(image: AssetImage(
                   'assets/images/logo.png'
--- lib/widgets.dart
+++ lib/widgets.dart
@@ -5,6 +5,11 @@ class TaskCardWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      padding: EdgeInsets.symmetric(
+        vertical: 32.0,
+        horizontal: 24.0,
+      ),
+      color: Colors.white,
       child: Text('Hello World'),
     );
   }
