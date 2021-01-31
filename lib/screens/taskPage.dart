94b7fde764d0bb72d807b33f20df6df937807aae
maneesha14w <maneesha14w@gmail.com>
2021-01-31 23:51:17 +0530

    added nav between pages

--- lib/screens/taskPage.dart
+++ lib/screens/taskPage.dart
@@ -1,5 +1,3 @@
-
-
 import 'package:flutter/material.dart';
 
 class TaskPage extends StatefulWidget {
@@ -11,9 +9,44 @@ class _TaskPageState extends State<TaskPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: Center(
-          child: Text("Hello World!"),
+      body: SafeArea(
+        child: Container(
+          child: Center(
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(24.0),
+                  child: Row(
+                    children: [
+                      InkWell(
+                        onTap: () {
+                          Navigator.pop(context);
+                        },
+                        child: Padding(
+                          padding: const EdgeInsets.all(24.0),
+                          child: Image(
+                            image:
+                                AssetImage('assets/images/back_arrow_icon.png'),
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                          child: TextField(
+                        decoration: InputDecoration(
+                          hintText: 'Enter Task Title...',
+                          border: InputBorder.none,
+                        ),
+                        style: TextStyle(
+                            fontSize: 26.0,
+                            fontWeight: FontWeight.bold,
+                            color: Color(0xFF211551)),
+                      ))
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
         ),
       ),
     );
